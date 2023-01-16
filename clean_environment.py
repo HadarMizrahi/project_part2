8831a40729e6cd0c1b4a8ebcce693ec5b886afcc
Hadar_Mi <hadarmizrahi7@gmail.com>
2023-01-16 22:39:27 +0200

    add files

--- clean_environment.py
+++ clean_environment.py
@@ -1,5 +1,6 @@
 import requests
 
+#The function stops the backend server
 def stop_backend_server():
     try:
         requests.get('http://127.0.0.1:5000/stop_server')
@@ -7,6 +8,7 @@ def stop_backend_server():
     except Exception:
         return 'Backend server is not responding'
 
+#The function stops the frontend server
 def stop_frontend_server():
     try:
         requests.get('http://127.0.0.1:5001/stop_server')
